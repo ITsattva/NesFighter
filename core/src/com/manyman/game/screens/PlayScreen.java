d3bb2def4d3b3102ebab3713de7835c5ad15f36e
itsattva <butthisattva@gmail.com>
2022-08-13 14:49:17 +0300

    it's time to save it somewhere

---
+++ core/src/com/manyman/game/screens/PlayScreen.java
@@ -0,0 +1,187 @@
+package com.manyman.game.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.manyman.game.ManymanGame;
+import com.manyman.game.scenes.Hud;
+import com.manyman.game.sprites.Mario;
+
+public class PlayScreen implements Screen {
+    private ManymanGame game;
+    private Hud hud;
+    public OrthographicCamera gamecam;
+    public Viewport gamePort;
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+    private World world;
+    private Box2DDebugRenderer b2dr;
+    private Mario player;
+
+
+    public PlayScreen(ManymanGame game) {
+        this.game = game;
+        gamecam = new OrthographicCamera();
+
+        gamePort = new FitViewport(ManymanGame.V_WIDTH  / ManymanGame.PPM, ManymanGame.V_HEIGHT / ManymanGame.PPM, gamecam);
+
+        hud = new Hud(game.batch);
+
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("level1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map, 1  / ManymanGame.PPM);
+
+        gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+
+
+        world = new World(new Vector2(0, -10), true);
+        b2dr = new Box2DDebugRenderer();
+
+        player = new Mario(world);
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+        Body body;
+
+        for (MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set((rect.getX() + rect.getWidth() / 2) / ManymanGame.PPM, (rect.getY() + rect.getHeight() / 2) / ManymanGame.PPM);
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2 / ManymanGame.PPM, rect.getHeight() / 2 / ManymanGame.PPM);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
+        for (MapObject object : map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set((rect.getX() + rect.getWidth() / 2) / ManymanGame.PPM, (rect.getY() + rect.getHeight() / 2) / ManymanGame.PPM);
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2 / ManymanGame.PPM, rect.getHeight() / 2 / ManymanGame.PPM);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
+        for (MapObject object : map.getLayers().get(5).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set((rect.getX() + rect.getWidth() / 2) / ManymanGame.PPM, (rect.getY() + rect.getHeight() / 2) / ManymanGame.PPM);
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2 / ManymanGame.PPM, rect.getHeight() / 2 / ManymanGame.PPM);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
+        for (MapObject object : map.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set((rect.getX() + rect.getWidth() / 2) / ManymanGame.PPM, (rect.getY() + rect.getHeight() / 2) / ManymanGame.PPM);
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2 / ManymanGame.PPM, rect.getHeight() / 2 / ManymanGame.PPM);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    private void handleInput(float dt) {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.W)) {
+            player.b2body.applyLinearImpulse(new Vector2(0, 4f), player.b2body.getWorldCenter(), true);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.D) && player.b2body.getLinearVelocity().x <= 2) {
+            player.b2body.applyLinearImpulse(new Vector2(0.1f, 0), player.b2body.getWorldCenter(), true);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.A) && player.b2body.getLinearVelocity().x >= -2) {
+            player.b2body.applyLinearImpulse(new Vector2(-0.1f, 0), player.b2body.getWorldCenter(), true);
+        }
+    }
+
+    public void update(float dt) {
+        handleInput(dt);
+
+        world.step(1 / 60f, 6, 2);
+
+        gamecam.position.x = player.b2body.getPosition().x;
+
+        gamecam.update();
+
+        renderer.setView(gamecam);
+    }
+
+
+    @Override
+    public void render(float delta) {
+        update(delta);
+
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        renderer.render();
+
+        b2dr.render(world, gamecam.combined);
+
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gamePort.update(width, height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ core/src/com/manyman/game/sprites/Mario.java
@@ -0,0 +1,33 @@
+package com.manyman.game.sprites;
+
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
+import com.manyman.game.ManymanGame;
+
+public class Mario {
+    public World world;
+    public Body b2body;
+
+    public Mario(World world) {
+        this.world = world;
+        defineMario();
+    }
+
+    private void defineMario() {
+        BodyDef bdef = new BodyDef();
+        bdef.position.set(32 / ManymanGame.PPM, 32 / ManymanGame.PPM);
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        b2body = world.createBody(bdef);
+
+        FixtureDef fdef = new FixtureDef();
+        CircleShape shape = new CircleShape();
+        shape.setRadius(5 / ManymanGame.PPM);
+
+        fdef.shape = shape;
+        b2body.createFixture(fdef);
+
+    }
+}
